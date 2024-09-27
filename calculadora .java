2f584b48d43edada084d0a5fd91c0af66df4d66c
bugj1 <rj031850@gmail.com>
2024-09-27 15:11:24 -0500

    multiplicacion

--- calculadora .java
+++ calculadora .java
@@ -3,6 +3,61 @@ public class calculadora{
 public int a;
 public int b;
 
+char choice;
+		System.out.println("MI CALCURADORA");
+		System.out.println("Escoja: ");
+		System.out.println("1. suma ");
+		System.out.println("2. resta");
+		System.out.println("3. potencia");
+		System.out.println("4. multiplicacion");
+		System.out.println("5. division");
+		
+		
+		
+		try {
+			choice = (char) System.in.read();
+			System.out.println("\n");
+			
+			switch(choice) {
+			
+			case '1' :
+				System.out.println("suma");
+				calculadora.suma();
+				
+				break;
+				
+			case '2' :
+				System.out.println("resta");
+				calculadora.resta();
+				break;
+				
+			case '3' :
+				System.out.println("potencia");
+				calculadora.potencia();
+				break;
+				
+			case '4' :
+				System.out.println("multiplicacion");
+				calculadora.multiplicacion();
+				break;
+				
+			case '5' :
+				System.out.println("division");
+				calculadora.division();
+				break;
+				
+			
+			default : 
+				System.out.println("La eleccion no existe");
+			
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("No fue posible la instruccion");
+		}
+
 public double suma(){
 
  double suma = a + b ;
@@ -27,6 +82,15 @@ public double potencia (){
     double potencia = a ** b;
      system.out.println("la potencia es : " + potencia );
      return potencia;
+}
+
+public double multiplicacion (){
+
+    double multiplicacion = a * b;
+    system.out.println("la multiplicacion es : " + multiplicacion );
+     return multiplicacion;
+
+
 }
 
 }
