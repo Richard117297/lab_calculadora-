c239a2a836a25fe39208ce700d075cd469af445a
Richard117297 <richardcarvajalb@gmail.com>
2024-09-27 17:08:55 -0500

    ajustes

--- calculadora .java
+++ calculadora .java
@@ -1,104 +1,105 @@
 public class calculadora{
 
-public int a;
-public int b;
-
-char choice;
-		System.out.println("MI CALCURADORA");
-		System.out.println("Escoja: ");
-		System.out.println("1. suma ");
-		System.out.println("2. resta");
-		System.out.println("3. potencia");
-		System.out.println("4. multiplicacion");
-		System.out.println("5. division");
-		
-		
-		
-		try {
-			choice = (char) System.in.read();
-			System.out.println("\n");
-			
-			switch(choice) {
-			
-			case '1' :
-				System.out.println("suma");
-				calculadora.suma();
-				
-				break;
-				
-			case '2' :
-				System.out.println("resta");
-				calculadora.resta();
-				break;
-				
-			case '3' :
-				System.out.println("potencia");
-				calculadora.potencia();
-				break;
-				
-			case '4' :
-				System.out.println("multiplicacion");
-				calculadora.multiplicacion();
-				break;
-				
-			case '5' :
-				System.out.println("division");
-				calculadora.division();
-				break;
-				
-			
-			default : 
-				System.out.println("La eleccion no existe");
-			
-			}
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("No fue posible la instruccion");
-		}
-
-public double suma(){
-
- double suma = a + b ;
-
- System.out.println("La suma es : " + suma);
-
- return suma;
-
-}
-
-public double resta (){
-    double resta = a - b ;
-    system.out.println("la resta es :  " + resta );
-    return resta;
-    
+ 	public int a;
+    public int b;
 
 
+    public calcuradora(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 
-}
-
-public double potencia (){
-    double potencia = a ** b;
-     system.out.println("la potencia es : " + potencia );
-     return potencia;
-}
-
-public double multiplicacion (){
-
-    double multiplicacion = a * b;
-    system.out.println("la multiplicacion es : " + multiplicacion );
-     return multiplicacion;
+    public static void main(String[] args) {
+    	
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese el valor de a: ");
+        int a = scanner.nextInt();
+        System.out.print("Ingrese el valor de b: ");
+        int b = scanner.nextInt();
 
 
-}
+        calcuradora calculadora = new calcuradora(a, b);
 
-public double division () {
+        
+        System.out.println("MI CALCULADORA");
+        System.out.println("Escoja: ");
+        System.out.println("1. suma ");
+        System.out.println("2. resta");
+        System.out.println("3. potencia");
+        System.out.println("4. multiplicación");
+        System.out.println("5. división");
 
-double division = a / b;
 
- system.out.println("la division es : " + dicivion );
-     return division;
-}
+        char choice = scanner.next().charAt(0);
+
+       
+        switch (choice) {
+            case '1':
+                System.out.println("Suma");
+                calculadora.suma();
+                break;
+                
+            case '2':
+                System.out.println("Resta");
+                calculadora.resta();
+                break;
+                
+            case '3':
+                System.out.println("Potencia");
+                calculadora.potencia();
+                break;
+                
+            case '4':
+                System.out.println("Multiplicación");
+                calculadora.multiplicacion();
+                break;
+                
+            case '5':
+                System.out.println("División");
+                calculadora.division();
+                break;
+                
+            default:
+                System.out.println("La elección no existe");
+                break;
+        }
+        
+        scanner.close();
+    }
+
+    public double suma() {
+        double suma = a + b;
+        System.out.println("La suma es: " + suma);
+        return suma;
+    }
+
+    public double resta() {
+        double resta = a - b;
+        System.out.println("La resta es: " + resta);
+        return resta;
+    }
+
+    public double potencia() {
+        double potencia = Math.pow(a, b);
+        System.out.println("La potencia es: " + potencia);
+        return potencia;
+    }
+
+    public double multiplicacion() {
+        double multiplicacion = a * b;
+        System.out.println("La multiplicación es: " + multiplicacion);
+        return multiplicacion;
+    }
+
+    public double division() {
+        if (b == 0) {
+            System.out.println("Error: División entre cero no permitida.");
+            return 0;
+        }
+        double division =  a / b;
+        System.out.println("La división es: " + division);
+        return division;
+    }
 
 }
